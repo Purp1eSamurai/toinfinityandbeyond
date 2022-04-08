a5f0e57058a7249a038a7d5f27b2bf500b213950
Purp1eSamurai <103236491+Purp1eSamurai@users.noreply.github.com>
2022-04-08 10:09:15 +0500

    Update test.sh

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -3,4 +3,17 @@ PATH=/etc:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 
 password_file='/etc/testuser.scrt'
 user='user1'
-ip='192.168.0.9'
+ip='192.168.31.0'
+
+apt update && apt upgrade -y
+apt install mc -y
+apt update && apt upgrade -y
+apt install unzip -y
+apt update && apt upgrade -y
+apt install curl -y
+apt update && apt upgrade -y
+apt install curl -y
+apt update && apt upgrade -y
+apt install wget -y
+apt update && apt upgrade -y
+apt install git -y
