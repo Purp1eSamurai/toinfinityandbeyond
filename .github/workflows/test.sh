a2c180d458a7969ea47408e9e2cb6904ab26c116
Purp1eSamurai <103236491+Purp1eSamurai@users.noreply.github.com>
2022-04-11 17:58:34 +0500

    Update test.sh

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -45,7 +45,7 @@ mysql -e "GRANT SELECT, INSERT, DELETE, UPDATE  ON database_test.* TO 'usertest'
 mysql -e "FLUSH PRIVILEGES;"
 mysql -e "CREATE TABLE IF NOT EXISTS database_test.orders (id INT, имя text, цена INT,PRIMARY KEY(id));"
 mysql -e "CREATE TABLE IF NOT EXISTS database_test.clients (id INT PRIMARY KEY, фамилия text, страна text, заказ INT, FOREIGN KEY(заказ) REFERENCES orders(id));"
-mysql -e "INSERT INTO database_test.orders VALUES (1, 'Chocolate', 10), (2, 'Printer', 3000), (3, 'Book', 500), (4, 'Monitor' 7000), (5, 'Guitar', 4000);"
- 
+mysql -e "INSERT IGNORE INTO database_test.orders VALUES (1, 'Шоколад', 10), (2, 'Принтер', 3000), (3, 'Книга', 500), (4, 'Монитор' 7000), (5, 'Гитара', 4000);"
+mysql -e "INSERT IGNORE INTO databa_test.clients VALUES (1, 'Иванов', 'USA',3), (2, 'Петров' 'Canada'2), (3, 'Chan' 'Japan',5), (4, 'Дмитриев', 'Russia',1),(5, 'Blackmore', 'Russia',4)
 
 
