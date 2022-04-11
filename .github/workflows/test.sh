4722c17dfe07d0e375730a5130df8b1b283063b1
Purp1eSamurai <103236491+Purp1eSamurai@users.noreply.github.com>
2022-04-11 14:11:21 +0500

    Update test.sh

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -11,7 +11,7 @@ sudo ufw allow in on "$if" to any port "$http"
 sudo ufw allow in on "$if" to any port "$https"
 sudo ufw allow in on "$if" to any port "$ssh" 
 sudo ufw allow in on lo
-sudo ufw default deny incoming
+sudo ufw default deny incoming 
 sudo ufw default allow outgoing
 sudo ufw --force enable
 sudo apt update
@@ -43,9 +43,8 @@ mysql -e "CREATE DATABASE IF NOT EXISTS database_test;"
 mysql -e "CREATE USER IF NOT EXISTS 'usertest'@'localhost' IDENTIFIED by '$pwd';"
 mysql -e "GRANT SELECT, INSERT, DELETE, UPDATE  ON database_test.* TO 'usertest'@'localhost';"
 mysql -e "FLUSH PRIVILEGES;"
-mysql -e "USE database_test;"
-mysql -e "CREATE TABLE IF NOT EXISTS orders (id INT PRIMARY KEY,  наименование character varying(100) ,  цена INT);"
-mysql -e "CREATE TABLE IF NOT EXISTS clients (id serial primary key, фамилия string, страна проживания string, index, заказ foreign key orders;"
+mysql -e "CREATE TABLE IF NOT EXISTS database_test.orders (id INT PRIMARY KEY,  наименование character varying(100) ,  цена INT);"
+mysql -e "CREATE TABLE IF NOT EXISTS database_test.clients (id serial primary key, фамилия string, страна проживания string, index, заказ foreign key orders);"
 
  
 
