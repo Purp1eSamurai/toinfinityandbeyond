df0e37f87ae84c77eead560a09e6440103d400de
Purp1eSamurai <103236491+Purp1eSamurai@users.noreply.github.com>
2022-04-12 13:19:03 +0500

    Update test.sh

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -38,13 +38,14 @@ pwd="$(date +%s | sha256sum | base64 | head -c 32)"
 sed -i "s/mypassword/$pwd/g"  /home/mysqluserpas
 
 
+
 mysql -e "CREATE DATABASE IF NOT EXISTS database_test;"
 mysql -e "CREATE USER IF NOT EXISTS 'usertest'@'localhost' IDENTIFIED by '$pwd';"
 mysql -e "GRANT SELECT, INSERT, DELETE, UPDATE  ON database_test.* TO 'usertest'@'localhost';"
 mysql -e "FLUSH PRIVILEGES;"
 mysql -e "CREATE TABLE IF NOT EXISTS database_test.orders (id INT, имя text, цена INT,PRIMARY KEY(id));"
 mysql -e "CREATE TABLE IF NOT EXISTS database_test.clients (id INT PRIMARY KEY, фамилия text, страна text, заказ INT, FOREIGN KEY(заказ) REFERENCES orders(id));"
-mysql -e "INSERT IGNORE INTO database_test.orders VALUES (1, 'Шоколад', 10), (2, 'Принтер', 3000), (3, 'Книга', 500), (4, 'Монитор' 7000), (5, 'Гитара', 4000);"
-mysql -e "INSERT IGNORE INTO databa_test.clients VALUES (1, 'Иванов', 'USA',3), (2, 'Петров' 'Canada'2), (3, 'Chan' 'Japan',5), (4, 'Дмитриев', 'Russia',1),(5, 'Blackmore', 'Russia',4)
+mysql -e "INSERT IGNORE INTO  database_test.orders VALUES (1, 'Шоколад', 10), (2, 'Принтер', 3000), (3, 'Книга', 500), (4, 'Монитор' 7000), (5, 'Гитара', 4000);"
+mysql -e "INSERT IGNORE INTO database_test.clients VALUES (1, 'Иванов', 'USA',3), (2, 'Петров' 'Canada'2), (3, 'Chan' 'Japan',5), (4, 'Дмитриев', 'Russia',1),(5, 'Blackmore', 'Russia',4);"
 
 
