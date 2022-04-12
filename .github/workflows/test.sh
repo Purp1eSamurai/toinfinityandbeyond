316e314777914223895ebfd6152a221cab5e2d28
Purp1eSamurai <103236491+Purp1eSamurai@users.noreply.github.com>
2022-04-12 11:52:04 +0500

    Update test.sh

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -38,7 +38,6 @@ pwd="$(date +%s | sha256sum | base64 | head -c 32)"
 sed -i "s/mypassword/$pwd/g"  /home/mysqluserpas
 
 
-sudo mysql -u root 
 mysql -e "CREATE DATABASE IF NOT EXISTS database_test;"
 mysql -e "CREATE USER IF NOT EXISTS 'usertest'@'localhost' IDENTIFIED by '$pwd';"
 mysql -e "GRANT SELECT, INSERT, DELETE, UPDATE  ON database_test.* TO 'usertest'@'localhost';"
